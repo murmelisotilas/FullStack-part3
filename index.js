76f22465b7a730dc2887cc2e1357d823b01d9fd1
murmelisotilas <105284237+murmelisotilas@users.noreply.github.com>
2022-05-31 10:44:02 +0300

    3.13.-3.14. part3

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express() 
+const Person = require('./models/person')
 
 let persons = [
     { 
@@ -25,6 +27,7 @@ let persons = [
       number: "39-23-6423122"
     }
 ]
+
 
 app.use(express.json())
 app.use(express.static('build'))
@@ -58,13 +61,15 @@ app.post('/api/persons', morgan(':url :method :body'),(req, res) => {
         })
     }
 
-    const person = {
+    const person = new Person({
         name: body.name,
         number: body.number,
-        id: generateId()
-    }
-    persons = persons.concat(person)
-    res.json(person)
+        id: generateId(),
+    })
+
+    person.save().then(savedPerson => {
+        res.json(savedPerson)
+    })
 })
 
 
@@ -76,17 +81,19 @@ app.delete('/api/persons/:id', (req, res) => {
 
 /*get all persons*/
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(persons => {
+        res.json(persons)
+    })
 })
+
 /*get person by id*/
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
+    Person.findById(req.params.id).then(person => {
         res.json(person)
-    } else {
+    })
+    .catch(error => {
         res.status(404).end()
-    }
+    })
 })
 
 /*info*/
@@ -106,4 +113,3 @@ const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`)
 })
-
